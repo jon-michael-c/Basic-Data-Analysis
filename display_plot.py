6c76d661fbea69f889d4823c05796721eb9a3a83
jon-michael-c <jonmichaelcubano@gmail.com>
2021-07-28 22:16:49 -0400

    python

---
+++ display_plot.py
@@ -0,0 +1,17 @@
+import numpy as np 
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+df = pd.read_csv('./summer.csv')
+df.head()
+
+ag = pd.DataFrame(df.groupby(['Country']).size().sort_values(ascending=False).head(10)).reset_index()
+ag.columns = ['Country','Medal']
+fig = go.Figure(go.Table(header=dict(values=ag.columns),
+                 cells=dict(values=[
+                     ag['Country'].to_list()
+                     , ag['Medal'].to_list()]
+                           )))
+fig.show()
+
--- list.awk
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/awk -F
-
-BEGIN {printf "%.3f\n", 2005.50 / 3}
